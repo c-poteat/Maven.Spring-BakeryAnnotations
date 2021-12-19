32c4c8198007eca9ec3115a2b488d5ae68738297
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-12-18 19:37:41 -0500

    Lab finished

--- src/main/java/com/zipcodewilmington/bakery/models/Baker.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Baker.java
@@ -1,15 +1,20 @@
 package com.zipcodewilmington.bakery.models;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Baker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
-
     private String employeeId;
-
     private String specialty;
 
     public Baker() {
--- src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
+++ src/main/java/com/zipcodewilmington/bakery/repositories/MuffinRepository.java
@@ -2,6 +2,10 @@ package com.zipcodewilmington.bakery.repositories;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Service
+@Repository
 public interface MuffinRepository extends CrudRepository<Muffin, Long> {
 }
